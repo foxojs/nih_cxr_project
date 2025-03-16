d557a2c461dab44997d96efb073b151d53b216a3
Ollie Fox <foxojs@gmail.com>
2025-03-16 21:33:35 +0000

    sh changes

--- shell_scripts/run_vit_cpu_training.sh
+++ shell_scripts/run_vit_cpu_training.sh
@@ -2,7 +2,7 @@
 #PBS -l select=1:ncpus=4:mem=100gb
 #PBS -N nih_cxr
 
-cd /rds/general/user/ojf24/home/ml_project/vit_model
+cd /rds/general/user/ojf24/home/ml_project/vit_base_model
 module load anaconda3/personal
 source activate tds_project
 
--- shell_scripts/run_vit_gpu_training.sh
+++ shell_scripts/run_vit_gpu_training.sh
@@ -2,10 +2,9 @@
 #PBS -l select=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
 #PBS -N nih_cxr
 
-cd /rds/general/user/ojf24/home/ml_project/training
+cd /rds/general/user/ojf24/home/ml_project/vit_base_model
 module load anaconda3/personal
 source activate tds_project
 
 
-python training_vit_scratch.py
-
+python main.py
