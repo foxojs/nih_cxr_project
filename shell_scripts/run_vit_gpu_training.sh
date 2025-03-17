6c8aacb9ae745283284bfdaee31df249dc1838c5
Ollie Fox <foxojs@gmail.com>
2025-03-17 11:33:45 +0000

    config file changes

--- shell_scripts/run_vit_gpu_training.sh
+++ shell_scripts/run_vit_gpu_training.sh
@@ -1,5 +1,5 @@
 #PBS -l walltime=2:00:00
-#PBS -l select=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
+#PBS -l select=1:ncpus=4:mem=100gb:ngpus=1:gpu_type=RTX6000
 #PBS -N nih_cxr_gpu
 
 cd /rds/general/user/ojf24/home/ml_project/vit_base_model
