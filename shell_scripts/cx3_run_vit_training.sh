3e924fb8787d1992eb1162eb001a2253031a05d9
Ollie Fox <foxojs@gmail.com>
2025-03-19 22:42:30 +0000

    remote changes

--- shell_scripts/cx3_run_vit_training.sh
+++ shell_scripts/cx3_run_vit_training.sh
@@ -1,5 +1,5 @@
 #PBS -l walltime=10:00:00
-#PBS -l select=1:ncpus=12:mem=40gb:ngpus=1
+#PBS -l select=1:ncpus=20:mem=40gb:ngpus=1
 #PBS -N cx3_nih_gpu
 
 cd /rds/general/user/ojf24/home/ml_project/vit_pretrained_fine_tune
