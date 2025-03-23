a874a12cb0e3fdaf377d29149e205bc8ad268a36
Ollie Fox <foxojs@gmail.com>
2025-03-23 19:24:51 +0000

    modifying shell scripts

--- shell_scripts/run_vit_gpu_training.sh
+++ shell_scripts/run_vit_gpu_training.sh
@@ -1,10 +1,10 @@
-#PBS -l walltime=20:00:00
-#PBS -l select=1:ncpus=4:mem=100gb:ngpus=1:gpu_type=RTX6000
+#PBS -l walltime=30:00:00
+#PBS -l select=1:ncpus=30:mem=200gb:ngpus=1:gpu_type=RTX6000
 #PBS -N nih_cxr_gpu
 
-cd /rds/general/user/ojf24/home/ml_project/vit_base_model
+cd /rds/general/user/ojf24/home/ml_project/vit_pretrained_fine_tune
 module load anaconda3/personal
-source activate tds_project
+source activate pytorch_env
 
 
 python main.py
