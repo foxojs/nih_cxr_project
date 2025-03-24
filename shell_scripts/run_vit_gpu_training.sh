8486c28a08a6c9e508ee75d8e284136f18d4617e
Ollie Fox <foxojs@gmail.com>
2025-03-24 17:16:13 +0000

    server changes

--- shell_scripts/run_vit_gpu_training.sh
+++ shell_scripts/run_vit_gpu_training.sh
@@ -1,5 +1,5 @@
-#PBS -l walltime=30:00:00
-#PBS -l select=1:ncpus=30:mem=200gb:ngpus=1:gpu_type=RTX6000
+#PBS -l walltime=2:00:00
+#PBS -l select=1:ncpus=30:mem=100gb:ngpus=1:gpu_type=RTX6000
 #PBS -N nih_cxr_gpu
 
 cd /rds/general/user/ojf24/home/ml_project/vit_pretrained_fine_tune
