5434f0945769e878d0ef9f15c85bf71e47b30aff
Ollie Fox <foxojs@gmail.com>
2025-03-25 09:29:20 +0000

    server updates

--- shell_scripts/cx3_run_vit_training.sh
+++ shell_scripts/cx3_run_vit_training.sh
@@ -1,5 +1,5 @@
-#PBS -l walltime=2:00:00
-#PBS -l select=1:ncpus=30:mem=80gb:ngpus=1
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=30:mem=100gb:ngpus=1
 #PBS -N cx3_nih_gpu
 
 cd /rds/general/user/ojf24/home/ml_project/vit_pretrained_fine_tune
